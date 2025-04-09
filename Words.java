b6c6cc7304e9169a1c6f0248fb03aca426cf8637
BernardinoCastro <erca.castro.up@phinmaed.com>
2025-04-09 20:05:08 +0800

    Update Words.java

--- Words.java
+++ Words.java
@@ -6,26 +6,26 @@ public class Words {
             "practice", "game", "random", "word", "falling", "letters",
             "code", "developer", "computer", "fast", "accurate", "learn",
             "debug", "compile", "execute", "syntax", "algorithm", "variable",
-            "function", "loop", "condition", "inheritance", "polymorphism", "encapsulation",
+            "function", "loop", "condition", "inheritance", "polymorphism",
             "object", "classloader", "recursion", "framework", "instance", "constructor",
-            "serialization", "deserialization", "multithreading", "lambda", "expression",
+            "serialization",  "lambda", "expression",
             "generic", "exception", "stack", "heap", "garbage", "collection",
             "pointer", "reference", "method", "interface", "parameter", "argument",
             "callback", "listener", "event", "asynchronous", "synchronous", "thread",
             "heap", "stacktrace", "buffer", "cache", "optimization", "refactor",
             "dependency", "library", "repository", "query", "database", "schema",
             "transaction", "commit", "rollback", "index", "primary", "foreign",
-            "normalization", "denormalization", "cluster", "shard", "node", "cloud",
+            "normalization", "cluster", "shard", "node", "cloud",
             "API", "REST", "endpoint", "token", "authentication", "authorization",
             "session", "cookie", "encryption", "decryption", "hashing", "salting",
             "firewall", "proxy", "router", "bandwidth", "latency", "protocol",
-            "packet", "socket", "DNS", "HTTP", "HTTPS", "TCP", "UDP",
-            "server", "client", "hosting", "deployment", "container", "virtualization"
+            "packet", "socket",
+            "server", "client", "hosting", "deployment", "container"
     };
 
     private static final String[] JAVA_KEYWORDS = {
             "public", "class", "static", "void", "int", "String",
-            "boolean", "System.out.println", "import", "new", "return",
+            "boolean", "import", "new", "return",
             "if", "else", "for", "while", "try", "catch", "final",
             "extends", "implements", "abstract", "interface", "package",
             "private", "protected", "default", "super", "this", "throw",
@@ -33,16 +33,14 @@ public class Words {
             "break", "continue", "case", "switch", "do", "instanceof",
             "strictfp", "native", "finally", "const", "goto", "module",
             "requires", "exports", "opens", "provides", "uses", "with",
-            "record", "sealed", "permits", "yield", "var", "try-with-resources",
+            "record", "sealed", "permits", "yield", "var",
             "annotation", "lambda", "stream", "optional", "reflection",
             "serialization", "functional", "predicate", "consumer", "supplier",
             "biFunction", "comparator", "singleton", "dependency", "autoboxing",
             "unboxing", "wildcard", "bounded", "erasure", "generic-type",
             "covariant", "contravariant", "diamond", "immutable", "mutable",
-            "method-reference", "concurrency", "executor", "future",
-            "completableFuture", "parallel", "fork-join", "thread-pool",
-            "garbage-collector", "finalizer", "soft-reference", "weak-reference",
-            "phantom-reference"
+            "concurrency", "executor", "future",
+             "parallel", "finalizer"
     };
 
     private static final Random RANDOM = new Random();
